85663cdb8cecac2df48c644dbf3c0ac34e8dabe9
Nik <nik.ignition.work@gmail.com>
2022-10-17 14:29:47 +0300

    app init

---
+++ src/com/academy/Main.java
@@ -0,0 +1,10 @@
+package com.academy;
+
+import com.academy.entity.Lecture;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Lecture lecture = new Lecture();
+    }
+}
---
+++ src/com/academy/entity/Lecture.java
@@ -0,0 +1,10 @@
+package com.academy.entity;
+
+public class Lecture {
+    private static long ID;
+
+
+    public Lecture() {
+        ID++;
+    }
+}
